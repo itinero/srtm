e929547b42a86c38bcec04c99503b980550a1715
Kurniawan Jatmika <munirjb@gmail.com>
2021-10-02 16:18:12 +0700

    remarking & cleanups

--- src/SRTM/Sources/SourceHelpers.cs
+++ src/SRTM/Sources/SourceHelpers.cs
@@ -30,6 +30,7 @@ namespace SRTM.Sources
       CredentialCache cache = new CredentialCache();
       cache.Add(new Uri(credentials.Domain), "Basic", new NetworkCredential(credentials.UserName, credentials.Password));
 
+      // Modify the simple client handler to using cached Credentials
       HttpClientHandler handler = new HttpClientHandler();
       handler.Credentials = cache;
       handler.CookieContainer = myContainer;
@@ -40,58 +41,10 @@ namespace SRTM.Sources
 
       return PerformDownload(client, local, remote, logErrors);
     }
-    //public static bool DownloadWithCredentials2(NetworkCredential credentials, string local, string remote, bool logErrors = false)
-    //{
-    //  var Logger = LogProvider.For<SourceHelpers>();
-    //  logErrors = true;
-
-    //  // Ideally the cookie container will be persisted to/from file
-    //  CookieContainer myContainer = new CookieContainer();
-
-    //  // Create a credential cache for authenticating when redirected to Earthdata Login
-    //  CredentialCache cache = new CredentialCache();
-    //  cache.Add(new Uri(credentials.Domain), "Basic", new NetworkCredential(credentials.UserName, credentials.Password));
-
-    //  try
-    //  {
-    //    if (File.Exists(local))
-    //    {
-    //      File.Delete(local);
-    //    }
-
-    //    // Execute the request
-    //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(remote);
-    //    request.Method = "GET";
-    //    request.Credentials = cache;
-    //    request.CookieContainer = myContainer;
-    //    request.PreAuthenticate = false;
-    //    request.AllowAutoRedirect = true;
-
-    //    // Now access the data
-    //    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-    //    using (Stream stream = response.GetResponseStream())
-    //    using (StreamReader reader = new StreamReader(stream))
-    //    using (var outputStream = File.OpenWrite(local))
-    //    {
-    //      stream.CopyTo(outputStream);
-    //    }
-
-    //    return true;
-    //  }
-    //  catch (Exception ex)
-    //  {
-    //    if (logErrors)
-    //    {
-    //      Logger.ErrorException("Download failed.", ex);
-    //    }
-    //  }
-    //  return false;
-    //}
 
     private static bool PerformDownload(HttpClient client, string local, string remote, bool logErrors = false)
     {
       var Logger = LogProvider.For<SourceHelpers>();
-      //logErrors = true;
 
       try
       {
